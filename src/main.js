877c618890e311c81c7c339565360435b9d6f862
ArkadiuszMajewski <119493816+ArkadiuszMajewski@users.noreply.github.com>
2023-06-05 22:14:27 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,4 +3,4 @@ import { searchForPhotos } from './handlers.js';
 const searchForm = document.querySelector('#search-form');
 
 searchForm.addEventListener('submit', searchForPhotos);
-window.addEventListener('scroll', scrollHandler);
+// window.addEventListener('scroll', scrollHandler);
