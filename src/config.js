83ba1d1f9c3939545813e3e2136e07c2aaef5147
ArkadiuszMajewski <119493816+ArkadiuszMajewski@users.noreply.github.com>
2023-06-05 22:12:23 +0200

    1

--- src/config.js
+++ src/config.js
@@ -1,10 +1,12 @@
-export const API_PATH = 'https://pixabay.com/api';
+export const API_PATH = 'https://pixabay.com/api/';
 export const API_KEY = '36819144-796cb24dbda7f1c215c0374a0';
 
 export const DEFAULT_PIXABAY_PARAMS = {
+  key: API_KEY,
   per_page: '20',
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: 'true',
-  key: API_KEY,
 };
+
+// '36819144-796cb24dbda7f1c215c0374a0
